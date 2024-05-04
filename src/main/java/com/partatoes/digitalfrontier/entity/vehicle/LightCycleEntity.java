0f56ab8eccbd78f870764b4f1d7124b713946db8
jperoutek <jperoutek@gmail.com>
2024-05-03 19:09:37 -0500

    Create bigger bounding box for lightcycle

--- src/main/java/com/partatoes/digitalfrontier/entity/vehicle/LightCycleEntity.java
+++ src/main/java/com/partatoes/digitalfrontier/entity/vehicle/LightCycleEntity.java
@@ -4,12 +4,26 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 
 public class LightCycleEntity extends Entity {
+    private static final double MODEL_WIDTH = 1;
+    private static final double MODEL_LENGTH = 3.5;
+    private static final double MODEL_HEIGHT = 1.5;
 
     public LightCycleEntity(EntityType<?> type, World world) {
         super(type, world);
+    }
+
+    @Override
+    public void setPosition(double x, double y, double z) {
+        this.setPos(x, y, z);
+        this.setBoundingBox(this.createBoundingBox(x, y, z));
+    }
+
+    private Box createBoundingBox(double x, double y, double z) {
+        return new Box(x, y + .2, z, x - LightCycleEntity.MODEL_WIDTH, y + LightCycleEntity.MODEL_HEIGHT, z + LightCycleEntity.MODEL_LENGTH);
     }
 
     @Override
