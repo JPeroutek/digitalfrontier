c24288d2a99b339423e270314e38252fdee09b5b
jperoutek <jperoutek@gmail.com>
2024-12-09 09:48:13 -0600

    add pixel block, doesn't do anything yet

---
+++ src/main/java/com/partatoes/digitalfrontier/block/custom/PixelBlock.java
@@ -0,0 +1,9 @@
+package com.partatoes.digitalfrontier.block.custom;
+
+import net.minecraft.block.Block;
+
+public class PixelBlock extends Block {
+    public PixelBlock(Settings settings) {
+        super(settings);
+    }
+}
